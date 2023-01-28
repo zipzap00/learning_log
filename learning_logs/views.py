e1dedd06b860bec5a282f819c6eab65d9cef0584
Bulat <halikob.b@yandex.ru>
2023-01-28 15:49:17 +0400

    Created a template

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+  """Домашняя страница приложения learning_log"""
+  return render(request, 'learning_logs/index.html')
