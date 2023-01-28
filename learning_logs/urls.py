7dbc10ce20ad54c38556f32c3fea6cdbce9eb84d
Bulat <halikob.b@yandex.ru>
2023-01-28 16:14:29 +0400

    Topic list page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,6 @@ app_name = 'learning_logs'
 urlpatterns = [
   # Домашняя страница
   path('', views.index, name='index'),
+  # Страница со списком всех тем.
+  path('topics/', views.topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
 
 def index(request):
   """Домашняя страница приложения learning_log"""
   return render(request, 'learning_logs/index.html')
+
+def topics(request):
+  """Выводит список тем."""
+  topics = Topic.objects.order_by('date_added')
+  context = {'topics': topics}
+  return render(request, 'learning_logs/topics.html', context)
